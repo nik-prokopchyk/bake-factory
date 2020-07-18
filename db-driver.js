daffb87b17f7ad16750ffd355c85349c631c5720
unknown <rurealtraders@gmail.com>
2020-07-18 12:56:24 +0300

    Первичный дизайн БД и бекенда

---
+++ db-driver.js
@@ -0,0 +1,71 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:.JJf25hGmDD!JyW@cluster0-terx7.mongodb.net/bake-factory?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("bake-factory").collection("stock");
+  // perform actions on the collection object
+  collection.insertOne({
+    waybill: 123456,
+    from: 'coca-cola',
+    to: 'me',
+    total: 5647,
+    goods: [
+      {
+        name: 'мука',
+        weight: 500,
+        amount: 3,
+        price: 32.25,
+
+      }
+    ]
+  }).then( res => {
+    console.log(res.result.ok);
+  })
+  // client.close();
+});
+
+class Receipt {
+  constructor(id, from, to, goods) {
+    this.id = id;
+    this.from = from;
+    this.to = to;
+    this.goods = goods;
+  }
+  log = () => {
+    console.log(  this.id, this.from, this.to, this.goods);
+  }
+}
+class Goods {
+  constructor(name, type, weight, amount, price) {
+    this.name = name;
+    this.type = type;
+    this.weight = weight;
+    this.amount = amount;
+    this.price = price;
+  }
+}
+
+let a = new Receipt(123, 'Mihyhkina E.A.', 'Prokopchuka N.A.', 'goods');
+a.log();
+
+class GoodsType {
+  constructor() {
+
+  }
+}
+/*
+Итого
+Сумма
+Цена без ПДВ
+Цена с  ПДВ
+Количество
+Вес
+Название продукта
+Кому
+От кого
+Номер накладной
+
+
+
+*/
