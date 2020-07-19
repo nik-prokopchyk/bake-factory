711038720da3911769c3df3e3d3b2cc3e7d9224d
unknown <bluebirdyourdestiny@gmail.com>
2020-07-19 11:45:29 +0300

    Добавлен тестовый роут

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const port = 3000
 const path = require('path')
 
 app.use('/assets', express.static('assets'))
+
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/index.html')))
-console.log(path.join(__dirname + '/assets/'));
+app.get('/lol', (req, res) => {
+
+  res.send({'kek': 'lol'})
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
