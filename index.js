5e429e081c08cb3706a8878be6f5f6efee35897e
unknown <bluebirdyourdestiny@gmail.com>
2020-07-26 20:08:18 +0300

    halfdone api

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const api = require('./api')
 
 app.use('/assets', express.static('assets'))
+app.use(express.json())
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/index.html')))
-app.get('/lol', (req, res) => {
 
+app.get('/lol', (req, res) => {
+  const data = new api.goods()
+  console.log(req.json())
   res.send({'Nik': 'pro'})
 })
+
+app.post('/lol', (req, res) => {
+  // const data = new api.goods()
+  // console.log(req.json({requestBody: req.body}))
+  console.log(req.body);
+  res.send({'Nik': 'pro'})
+})
+
 app.get('/cek', (req, res) => {
   res.send('my name is Nik')
 })
